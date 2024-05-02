2e5a080d890eab1de4a58c40ef868afd7bf44727
cilvia333 <cilvia@kunshiran.design>
2024-05-02 21:45:49 +0900

    update: Presentation の処理を修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,22 @@
 import type { Config } from "drizzle-kit";
 
-export default {
-  schema: "./src/infrastructure/drizzle/schema.ts",
-  out: "./migrations",
-  driver: "d1",
-  dbCredentials: {
-    wranglerConfigPath: "wrangler.toml",
-    dbName: "hono-ddd",
-  },
-} satisfies Config;
+const { LOCAL_DB_PATH } = process.env;
+
+export default LOCAL_DB_PATH
+  ? ({
+      schema: "./src/infrastructure/drizzle/schema.ts",
+      out: "./migrations",
+      driver: "better-sqlite",
+      dbCredentials: {
+        url: LOCAL_DB_PATH,
+      },
+    } satisfies Config)
+  : ({
+      schema: "./src/infrastructure/drizzle/schema.ts",
+      out: "./migrations",
+      driver: "d1",
+      dbCredentials: {
+        wranglerConfigPath: "wrangler.toml",
+        dbName: "hono-ddd",
+      },
+    } satisfies Config);
--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 
 import { Bindings } from "@/types/hono";
 import { DrizzleBookRepository } from "@/infrastructure/drizzle/Book/DrizzleBookRepository";
@@ -7,39 +9,46 @@ import {
   RegisterBookApplicationService,
   RegisterBookCommand,
 } from "@/action/Book/RegisterBookApplicationService/RegisterBookApplicationService";
-import { HTTPException } from "hono/http-exception";
 
 const bookApp = new Hono<{ Bindings: Bindings }>();
 
-bookApp.get("/", (c) => {
+bookApp.get("/", (c, next) => {
   return c.text("hello book api!");
 });
-bookApp.post("/", async (c, next) => {
-  try {
-    const drizzleClient = getDrizzleClient(c.env.DB);
+bookApp.post(
+  "/",
+  zValidator(
+    "json",
+    z.object({
+      isbn: z.string(),
+      title: z.string(),
+      priceAmount: z.number().int(),
+    })
+  ),
+  async (c) => {
+    try {
+      const drizzleClient = getDrizzleClient(c.env.DB);
+      const requestBody = c.req.valid("json");
 
-    const requestBody = await c.req.json<{
-      isbn: string;
-      title: string;
-      priceAmount: number;
-    }>();
+      // リクエストボディをコマンドに変換。今回はたまたま一致しているため、そのまま渡している。
+      const registerBookCommand: RegisterBookCommand = requestBody;
+      await RegisterBookApplicationService.execute(
+        DrizzleBookRepository,
+        drizzleClient
+      )(registerBookCommand);
 
-    // リクエストボディをコマンドに変換。今回はたまたま一致しているため、そのまま渡している。
-    const registerBookCommand: RegisterBookCommand = requestBody;
-    await RegisterBookApplicationService.execute(
-      DrizzleBookRepository,
-      drizzleClient
-    )(registerBookCommand);
-
-    // 実際は詳細なレスポンスを返す
-    c.json({ message: "success" }, 200);
-  } catch (error) {
-    // 実際はエラーを解析し、詳細なレスポンスを返す。また、ロギングなどを行う。
-    throw new HTTPException(500, {
-      message: (error as Error).message,
-    });
+      // 実際は詳細なレスポンスを返す
+      return c.json({ message: "success" });
+    } catch (error) {
+      // 実際はエラーを解析し、詳細なレスポンスを返す。また、ロギングなどを行う。
+      return c.json(
+        {
+          message: (error as Error).message,
+        },
+        400
+      );
+    }
   }
-  await next();
-});
+);
 
 export default bookApp;
